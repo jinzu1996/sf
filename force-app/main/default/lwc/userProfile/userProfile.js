93951239428e1d219e0eb707e46a51fd455d3483
Jo <jinzu1996@gmail.com>
2022-10-14 17:32:24 +0530

    Max

--- force-app/main/default/lwc/userProfile/userProfile.js
+++ force-app/main/default/lwc/userProfile/userProfile.js
@@ -1,18 +1,16 @@
-import { LightningElement,wire, api} from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import getUserImageUrl from '@salesforce/apex/UserProfileController.getUserImageUrl';
 import updateUserProfilePic from '@salesforce/apex/FilesController.updateUserProfilePic';
 import userId from '@salesforce/user/Id';
-import {NavigationMixin} from 'lightning/navigation';
 import { refreshApex } from '@salesforce/apex';
 import basePath from '@salesforce/community/basePath';
 import getFields from '@salesforce/apex/FieldsetControllerRecordForm.getFields';
-const FIELDS = ['User.FirstName', 'User.LastName'];
-export default class UserProfile extends NavigationMixin(LightningElement) {
+export default class UserProfile extends LightningElement {
     @api profilelabel;
     @api fieldset;
 
+
     profileImageLoading = false;
-    
     fields = [];
     isReadOnly = true;
     error;
@@ -21,85 +19,75 @@ export default class UserProfile extends NavigationMixin(LightningElement) {
     photoUrlresponse;
     imageData;
     communityHome = basePath;
-    @wire(getUserImageUrl) 
-    mediumphotourl(result){
+
+    @wire(getUserImageUrl)
+    mediumphotourl(result) {
         this.photoUrlresponse = result;
         this.photoUrl = result.data;
     };
 
-    
-    connectedCallback()
-    {
-        getFields({ objectAPIName:'User', fieldSetAPIName:this.fieldset})
-        .then(result => {
-            if (result) {
-                this.fields = result;
-                this.error = undefined;
-             }
-        }) .catch(error => {
-            console.log(error);
-            this.error = error;
-        }); 
+    connectedCallback() {
+        if (!this.profilelabel) {
+            this.profilelabel = 'My Profile';
+        }
+
+        getFields({ objectAPIName: 'User', fieldSetAPIName: this.fieldset })
+            .then(result => {
+                if (result) {
+                    this.fields = result;
+                    this.error = undefined;
+                }
+            }).catch(error => {
+                console.log(error);
+                this.error = error;
+            });
     }
 
-    handleHome(event){
+    handleEditField(event) {
         event.preventDefault();
-        this[NavigationMixin.Navigate]({
-            type: 'standard__namedPage',
-            attributes: {
-                pageName: 'Home'
-            },
-        });
-
-    }
-
-    handleEditField(event){
-        event.preventDefault();
+        console.log('>> handleEditField');
         this.isReadOnly = false;
     }
 
-    handleSuccess(event){
+    handleSuccess(event) {
         this.isReadOnly = true;
     }
 
-    
+    openfileUploadedit(event) {
+        event.preventDefault();
+        console.log('>> openfileUploadedit');
+        let input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = _ => {
 
-openfileUploadedit(event) {
-    event.preventDefault();
-    let input = document.createElement('input');
-  input.type = 'file';
-  input.onchange = _ => {
-    // you can use this method to get file and perform respective operations
-    //const file = event.target.files[0];
-    let file =   input.files[0];
-    console.log(file);
-    var reader = new FileReader();
-    var base64;
-    reader.onload = () =>  {
-        
-        console.log('inside onload');
-        base64 = reader.result.split(/,/)[1];
-        //console.log(base64);
-        this.imageData = base64;
-        this.profileImageLoading = true;
-        updateUserProfilePic({base64 : this.imageData}).then(result => {
+            let file = input.files[0];
+            console.log(file);
+            var reader = new FileReader();
+            var base64;
+            reader.onload = () => {
 
-            if (result) {
-                console.log('image upload successful - openfileUploadedit');
-                    this.profileImageLoading = false;
-                    return refreshApex(this.photoUrlresponse);
+                console.log('reader.onload');
+                base64 = reader.result.split(/,/)[1];
+                this.imageData = base64;
+                this.profileImageLoading = true;
+                updateUserProfilePic({ base64: this.imageData }).then(result => {
+
+                    if (result) {
+                        console.log('image upload successful - openfileUploadedit');
+                        this.profileImageLoading = false;
+                        return refreshApex(this.photoUrlresponse);
                     }
-        }) .catch(error => {
-            console.log(error);
-            this.error = error;
-        }); 
-          }
-      reader.readAsDataURL(file);
-      refreshApex(this.photoUrl);
-            };
-        console.log("Before  click");
-  input.click();
-    
-    
-}
+                }).catch(error => {
+                    console.log(error);
+                    this.error = error;
+                });
+            }
+            reader.readAsDataURL(file);
+            
+        };
+
+        input.click();
+
+
+    }
 }
